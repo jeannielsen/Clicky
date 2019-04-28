a2d5b5112b1b4b5d4b224573dc7927b30d28f3e1
Nielsen <jeanielsen@comcast.net>
2019-04-28 16:56:55 -0700

    Updates

---
+++ precache-manifest.ce980b1889eb94669fd8d5a73309819c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "444b8d7ff8bb36a0e83f4c6198408c4b",
+    "url": "/clicky/index.html"
+  },
+  {
+    "revision": "e7687965df704f479a14",
+    "url": "/clicky/static/css/main.5b99c8d1.chunk.css"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/clicky/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "e7687965df704f479a14",
+    "url": "/clicky/static/js/main.fb7cdd18.chunk.js"
+  },
+  {
+    "revision": "e4363465db862e89c37e",
+    "url": "/clicky/static/js/runtime~main.c6956f21.js"
+  }
+]);
